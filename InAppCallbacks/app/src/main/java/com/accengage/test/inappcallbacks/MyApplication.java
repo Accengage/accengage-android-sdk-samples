bc60e0a3bb3a893779b1609f0d7274d16182a115
Andrei Mandychev <amandychev@accengage.com>
2016-05-26 10:20:37 +0200

    InAppCallbacks example is added (for popup)

---
+++ InAppCallbacks/app/src/main/java/com/accengage/test/inappcallbacks/MyApplication.java
@@ -0,0 +1,9 @@
+package com.accengage.test.inappcallbacks;
+
+import android.app.Application;
+
+import com.a4s.sdk.plugins.annotations.UseA4S;
+
+@UseA4S
+public class MyApplication extends Application {
+}
