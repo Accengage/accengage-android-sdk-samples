b2a23f7cd0d533449969fdee413100c9c7e0f6ce
Andrei Mandychev <amandychev@accengage.com>
2016-06-07 17:42:59 +0200

    MultiAction sample is added for in-apps

---
+++ InAppMultiAction/app/src/main/java/com/accengage/test/inappmultiaction/MainActivity.java
@@ -0,0 +1,100 @@
+package com.accengage.test.inappmultiaction;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.a4s.sdk.plugins.annotations.UseA4S;
+import com.ad4screen.sdk.A4S;
+import com.ad4screen.sdk.InApp;
+
+@UseA4S
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private InApp mInApp = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        A4S.get(this).setInAppInflatedCallback(new A4S.Callback<InApp>() {
+            @Override
+            public void onResult(InApp inapp) {
+                Log.d(TAG, "InAppInflatedCallback");
+                mInApp = inapp;
+                FrameLayout layout = inapp.getLayout();
+
+                WebView webview = (WebView) layout.findViewById(R.id.com_ad4screen_sdk_webview);
+                if (webview != null) {
+                    webview.setOnTouchListener(new View.OnTouchListener() {
+                        @Override
+                        public boolean onTouch(View v, MotionEvent event) {
+                            com.ad4screen.sdk.Log.debug("motion=" + event.getAction());
+                            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                                Toast.makeText(MainActivity.this, "Zone#WebView, track CLICK", Toast.LENGTH_SHORT).show();
+                                mInApp.setClickZone("Zone#WebView");
+                                mInApp.handleClick();
+                            }
+                            return false;
+                        }
+                    });
+                }
+
+                Button buttonInform = (Button) layout.findViewById(R.id.btn_inform);
+                buttonInform.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "Zone1, track CLICK", Toast.LENGTH_SHORT).show();
+                        mInApp.setClickZone("Zone1");
+                        mInApp.handleClick();
+                    }
+                });
+
+                Button buttonLater = (Button) layout.findViewById(R.id.btn_later);
+                buttonLater.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "Zone2, track CLICK", Toast.LENGTH_SHORT).show();
+                        mInApp.setClickZone("Zone2");
+                        mInApp.handleClick();
+                    }
+                });
+
+                ImageView iv = (ImageView) layout.findViewById(R.id.iv_close);
+                iv.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "Zone#Close, track CLOSE", Toast.LENGTH_SHORT).show();
+                        mInApp.setClickZone("Zone#Close");
+                        mInApp.dismiss();
+                    }
+                });
+
+                LinearLayout layoutBanner = (LinearLayout) layout.findViewById(R.id.ll_banner);
+                layoutBanner.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "The zone is not used", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onError(int i, String s) {
+
+            }
+        });
+
+    }
+}
