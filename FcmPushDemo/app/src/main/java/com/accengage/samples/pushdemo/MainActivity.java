44ca195ef35a0f032a4e1698acfad155e4ad9ea5
Andrei Mandychev <amandychev@accengage.com>
2017-01-11 16:18:35 +0100

    Add FcmPushDemo sample

---
+++ FcmPushDemo/app/src/main/java/com/accengage/samples/pushdemo/MainActivity.java
@@ -0,0 +1,34 @@
+package com.accengage.samples.pushdemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ad4screen.sdk.A4S;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        A4S.get(this).setIntent(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        A4S.get(this).startActivity(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        A4S.get(this).stopActivity(this);
+    }
+}
---
+++ FcmPushDemo/app/src/main/java/com/accengage/samples/pushdemo/MyApplication.java
@@ -0,0 +1,28 @@
+package com.accengage.samples.pushdemo;
+
+import android.content.res.Configuration;
+
+import com.ad4screen.sdk.A4SApplication;
+
+public class MyApplication extends A4SApplication {
+
+    @Override
+    public void onApplicationCreate() {
+        // ...
+    }
+
+    @Override
+    public void onApplicationTerminate() {
+        // ...
+    }
+
+    @Override
+    public void onApplicationLowMemory() {
+        // ...
+    }
+
+    @Override
+    public void onApplicationConfigurationChanged(Configuration newConfig) {
+        // ...
+    }
+}
