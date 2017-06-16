45db54706b0c21a081d34fce4f005bf968bbdddc
Kevin GILLES <kgilles@ad4s.local>
2017-06-16 11:45:07 +0200

    Modified AccCustomInApps Renamed rood folder to match naming rules Changed app logo from default to accengage's Changed the root ReadMe file accordingly Updated the required dependencies

---
+++ AccCustomInApps/app/src/main/java/com/accengage/samples/custominapps/customviews/BannerDownView.java
@@ -0,0 +1,90 @@
+package com.accengage.samples.custominapps.customviews;
+
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.accengage.samples.custominapps.R;
+import com.ad4screen.sdk.InApp;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.RequestCreator;
+
+import java.util.HashMap;
+
+/**
+ * Template used: banner_down
+ *
+ * Associated custom parameters:
+ * - inapp_title: Title text
+ * - inapp_text1: Content text
+ * - inapp_cta: Call to Action text
+ * - inapp_img: Url of a distant image
+ */
+public class BannerDownView extends CustomInAppLayout {
+
+    private TextView mTvTitle;
+    private TextView mTvDesc;
+    private TextView mTvCTA;
+    private ImageView mImageView;
+    private ImageView mCloseButton;
+
+    public BannerDownView(Context context) {
+        super(context);
+        initViews();
+    }
+
+    public BannerDownView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initViews();
+    }
+
+    public BannerDownView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initViews();
+    }
+
+    private void initViews() {
+        inflate(getContext(), R.layout.banner_down, this);
+
+        mTvTitle = (TextView) findViewById(R.id.title);
+        mTvDesc = (TextView) findViewById(R.id.description);
+        mTvCTA = (TextView) findViewById(R.id.cta);
+        mImageView = (ImageView) findViewById(R.id.imageIcon);
+        mCloseButton = (ImageView) findViewById(R.id.close_banner);
+    }
+
+    @Override
+    public void populate(InApp inApp) {
+        super.populate(inApp);
+
+        HashMap<String, String> customParameters = inApp.getCustomParameters();
+        mTvTitle.setText(customParameters.get("inapp_title"));
+        mTvDesc.setText(customParameters.get("inapp_text1"));
+        mTvCTA.setText(customParameters.get("inapp_cta"));
+
+        mCloseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mInApp.dismiss();
+            }
+        });
+
+        mTvCTA.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mInApp.handleClick();
+                Toast.makeText(getContext(), "BannerDown was clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        String imageLink = customParameters.get("inapp_img");
+        if (!imageLink.isEmpty()) {
+            RequestCreator requestCreator = Picasso.with(getContext()).load(imageLink);
+            requestCreator.into(mImageView);
+        }
+    }
+}
---
+++ AccCustomInApps/app/src/main/java/com/accengage/samples/custominapps/customviews/PopupBasicView.java
@@ -0,0 +1,95 @@
+package com.accengage.samples.custominapps.customviews;
+
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.accengage.samples.custominapps.R;
+import com.ad4screen.sdk.InApp;
+
+import java.util.HashMap;
+
+/**
+ * Template used: popup_basic
+ *
+ * Associated custom parameters:
+ * - inapp_title: Title text
+ * - inapp_text1: Content title
+ * - inapp_cta: Call to action text
+ */
+public class PopupBasicView extends CustomInAppLayout {
+
+    private TextView mTvTitle;
+    private TextView mTvDesc;
+    private TextView mTvCTA;
+    private ImageView mCloseButton;
+    private View mPopupCoreView;
+
+    public PopupBasicView(Context context) {
+        super(context);
+        initViews();
+    }
+
+    public PopupBasicView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initViews();
+    }
+
+    public PopupBasicView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initViews();
+    }
+
+    private void initViews() {
+        inflate(getContext(), R.layout.popup_basic, this);
+
+        mTvTitle = (TextView) findViewById(R.id.title);
+        mTvDesc = (TextView) findViewById(R.id.text1);
+        mTvCTA = (TextView) findViewById(R.id.cta);
+        mCloseButton = (ImageView) findViewById(R.id.close_popup);
+        mPopupCoreView = findViewById(R.id.popup_core);
+    }
+
+    @Override
+    public void populate(final InApp inApp) {
+        super.populate(inApp);
+        HashMap<String, String> customParameters = inApp.getCustomParameters();
+
+        mTvTitle.setText(customParameters.get("inapp_title"));
+        mTvDesc.setText(customParameters.get("inapp_text1"));
+        mTvCTA.setText(customParameters.get("inapp_cta"));
+
+        mCloseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inApp.dismiss();
+            }
+        });
+
+        mTvCTA.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inApp.handleClick();
+                Toast.makeText(getContext(), "BannerDown was clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        this.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inApp.dismiss();
+            }
+        });
+
+        mPopupCoreView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Nothing
+            }
+        });
+    }
+}
