7593cf7ce5e4f353ce929b7b79e5ae86f7190abe
Andrei Mandychev <amandychev@accengage.com>
2017-06-30 15:36:19 +0200

    AccInboxFirebase: use InboxNavActivity

--- AccInboxFirebase/app/src/main/java/com/accengage/samples/inbox/accinboxfirebase/MainActivity.java
+++ AccInboxFirebase/app/src/main/java/com/accengage/samples/inbox/accinboxfirebase/MainActivity.java
@@ -1,7 +1,11 @@
 package com.accengage.samples.inbox.accinboxfirebase;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.accengage.samples.inbox.InboxNavActivity;
+import com.ad4screen.sdk.A4S;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(this, InboxNavActivity.class));
+        finish();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        A4S.get(this).setIntent(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        A4S.get(this).startActivity(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        A4S.get(this).stopActivity(this);
     }
 }
