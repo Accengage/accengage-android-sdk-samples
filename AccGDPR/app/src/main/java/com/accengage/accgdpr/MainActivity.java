a48ffb8143800354b9bb97bb26feba6efebb0d69
Anthony <acadet@accengage.com>
2018-05-16 15:32:20 +0200

    [AccGDPR]

---
+++ AccGDPR/app/src/main/java/com/accengage/accgdpr/MainActivity.java
@@ -0,0 +1,63 @@
+package com.accengage.accgdpr;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SwitchCompat;
+
+import com.ad4screen.sdk.A4S;
+import com.ad4screen.sdk.OptinType;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnCheckedChanged;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.switch_optin_data) SwitchCompat switchOptinData;
+    @BindView(R.id.switch_optin_geoloc) SwitchCompat switchOptinGeoloc;
+
+    @OnCheckedChanged(R.id.switch_optin_data)
+    void onOptinDataCheckedChanged() {
+        A4S.get(this).setOptinData(this, switchOptinData.isChecked() ? OptinType.YES : OptinType.NO);
+    }
+
+    @OnCheckedChanged(R.id.switch_optin_geoloc)
+    void onOptinGeolocCheckedChanged() {
+        A4S.get(this).setOptinGeoloc(this, switchOptinGeoloc.isChecked() ? OptinType.YES : OptinType.NO);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        A4S.get(this).setIntent(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initView();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        A4S.get(this).startActivity(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        A4S.get(this).stopActivity(this);
+    }
+
+    private void initView() {
+        OptinType isOptinData = A4S.get(this).getOptinDataStatus(this);
+        OptinType isOptinGeoloc = A4S.get(this).getOptinGeolocStatus(this);
+
+        switchOptinData.setChecked(OptinType.YES.equals(isOptinData));
+        switchOptinGeoloc.setChecked(OptinType.YES.equals(isOptinGeoloc));
+    }
+}
